e347cffb4bebd5fdab152c7b94ee757ecfcb1449
utubun <dmytro.strunin@gmail.com>
2022-10-12 23:06:26 +0200

    UI: Implemented simple PathwayCard component

---
+++ components/cards/pathway/PathwayCard.mocks.ts
@@ -0,0 +1,11 @@
+import { IPathwayCard } from "./PathwayCard";
+
+const base: IPathwayCard = {
+    tag: "pathway",
+    title: "Quorum Sensing",
+    body: 'Summary of the pathway'
+};
+
+export const mockPathwayCardProps = {
+    base,
+}
