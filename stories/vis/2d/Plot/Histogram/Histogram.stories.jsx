8031f7e921ee4596d87a1ba39ebbbabf892c8770
utubun <dmytro.strunin@gmail.com>
2022-10-18 21:36:53 +0200

    UI: Build histogram and barplots. Fixed reploting on the same svg by removing the children

--- stories/vis/2d/Plot/Histogram/Histogram.stories.jsx
+++ stories/vis/2d/Plot/Histogram/Histogram.stories.jsx
@@ -4,24 +4,24 @@ import Histogram from './Histogram';
 const data = Array.from({ length: 100 }, () => Math.floor(Math.random() * 100));
 
 const margin = {
-    top: 20,
-    right: 5,
-    bottom: 30,
-    left: 40,
-}
+  top: 20,
+  right: 5,
+  bottom: 30,
+  left: 40,
+};
 
 const Template = (args) => <Histogram {...args} />;
 
 export const SimpleHistogram = Template.bind({});
 
 SimpleHistogram.args = {
-    data: data,
-    width: 300,
-    height: 300,
-    margin: margin
-}
+  data: data,
+  width: 300,
+  height: 300,
+  margin: margin,
+};
 
 export default {
-    title: 'Visualizations/2D/SimpleHistogram',
-    component: SimpleHistogram
+  title: 'Visualizations/2D/SimpleHistogram',
+  component: SimpleHistogram,
 };
--- stories/vis/2d/shapes/shapes.jsx
+++ stories/vis/2d/shapes/shapes.jsx
@@ -2,47 +2,38 @@ import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 
 const Shapes = ({ data, width, height, margin }) => {
+  const d3Container = useRef(null);
 
-    const d3Container = useRef(null);
+  useEffect(() => {
+    const svg = d3
+      .select(d3Container.current)
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('width', width - margin.left - margin.right)
+      .attr('height', height - margin.top, margin.bottom)
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    useEffect(
-        () => {
-            const svg = d3.select(d3Container.current)
-              .attr('width', width)
-              .attr('height', height)
-              .append('g')
-                .attr('width', width - margin.left - margin.right)
-                .attr('height', height - margin.top, margin.bottom)
-                .attr(
-                    'transform',
-                    `translate(${margin.left}, ${margin.top})`
-                );
-            
-            const update = svg
-              .selectAll('circle')
-              .data(data)
-              .join('circle')
-                .attr('r', (d) => d.r)
-                .attr('cx', (d) => d.cx)
-                .attr('cy', (d) => d.cy)
-              .style('fill', (d) => (d.r % 2 ? 'orange' : '#eee'));
+    const update = svg
+      .selectAll('circle')
+      .data(data)
+      .join('circle')
+      .attr('r', (d) => d.r)
+      .attr('cx', (d) => d.cx)
+      .attr('cy', (d) => d.cy)
+      .style('fill', (d) => (d.r % 2 ? 'orange' : '#eee'));
 
-            update.exit()
-              .remove();
+    update.exit().remove();
+  }, [data, d3Container.current]);
 
-        },
-        [ data, d3Container.current ]
-    );
-
-    return (
-        <svg
-          className = 'd3-component'
-          width = { width }
-          height = { height }
-          ref = { d3Container }
-        />
-    )
-
+  return (
+    <svg
+      className="d3-component"
+      width={width}
+      height={height}
+      ref={d3Container}
+    />
+  );
 };
 
 export default Shapes;
